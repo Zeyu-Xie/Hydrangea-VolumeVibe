8ec4cc3e5146dc2340eb52fe4e14afbb37f2f093
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-04-14 18:34:04 +0800

    .

--- VolumeVibe/ContentView.swift
+++ VolumeVibe/ContentView.swift
@@ -15,21 +15,37 @@ struct ContentView: View {
     private var listener = VolumeListener { _volume in
         print(_volume)
     }
-    
+
     init() {
         setSysVolum(0.002)
     }
 
     var body: some View {
         VStack {
-            Image(systemName: "info.circle").font(.system(size: 50)).foregroundStyle(.red)
+            Image(systemName: "info.circle").font(.system(size: 60)).foregroundStyle(.blue)
             Spacer().frame(maxHeight: 20)
             Text("VolumeVibe").bold().font(.title3)
             Text("Acan Xie").foregroundStyle(.secondary)
             Spacer().frame(maxHeight: 30)
             Text("Version 1.0.0").foregroundStyle(.secondary)
-        }
-        .padding()
+            Text("iOS" + " " + UIDevice.current.systemVersion).foregroundStyle(.secondary)
+            Spacer().frame(maxHeight: 30)
+            Text("You can press the button to set the volume to 0.002.").foregroundStyle(.secondary)
+            Spacer().frame(maxHeight: 20)
+            Button(action: {
+                // 按钮点击时的操作
+            }) {
+                Text("Click It")
+                    .frame(maxWidth: .infinity)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding(12)
+                    .background(.blue)
+                    .cornerRadius(10)
+            }
+            Spacer().frame(maxHeight: 20)
+            Text("Note: this app may not work on early iOS versions.").foregroundStyle(.secondary).font(.subheadline)
+        }.padding(40).frame(alignment: .center).multilineTextAlignment(.center)
     }
 }
 
