9c048d0a000793dc9cf06a3a10b9671529d25460
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-04-14 15:54:09 +0800

    .

--- VolumeVibe/ContentView.swift
+++ VolumeVibe/ContentView.swift
@@ -5,15 +5,29 @@
 //  Created by 谢泽钰 on 2024/4/14.
 //
 
+import AVFoundation
+import MediaPlayer
 import SwiftUI
 
 struct ContentView: View {
+    @State private var volume = 0
+
+    private var listener = VolumeListener { _volume in
+        print(_volume)
+    }
+    
+    init() {
+        setSysVolum(0.002)
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "info.circle").font(.system(size: 50)).foregroundStyle(.red)
+            Spacer().frame(maxHeight: 20)
+            Text("VolumeVibe").bold().font(.title3)
+            Text("Acan Xie").foregroundStyle(.secondary)
+            Spacer().frame(maxHeight: 30)
+            Text("Version 1.0.0").foregroundStyle(.secondary)
         }
         .padding()
     }
---
+++ VolumeVibe/VolumeVibeFunctions.swift
@@ -0,0 +1,64 @@
+//
+//  VolumeVibeFunctions.swift
+//  VolumeVibe
+//
+//  Created by 谢泽钰 on 2024/4/14.
+//
+
+import Foundation
+import MediaPlayer
+
+func setSysVolum(_ value: Float) {
+    let volumeView = MPVolumeView()
+    if let view = volumeView.subviews.first as? UISlider {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+            view.value = value
+        }
+    }
+}
+
+class VolumeListener: NSObject {
+    private var audioSession: AVAudioSession!
+    private var volumeObservation: NSKeyValueObservation?
+
+    private var volumeDidChange: (_ volume: Float) -> Void // 音量变化通知函数
+
+    init(volumeDidChange: @escaping (_ volume: Float) -> Void) {
+        self.volumeDidChange = volumeDidChange
+        super.init()
+        setupAudioSession()
+    }
+
+    private func setupAudioSession() {
+        audioSession = AVAudioSession.sharedInstance()
+
+        do {
+            try audioSession.setActive(true, options: [])
+        } catch {
+            print("Failed to activate audio session: \(error)")
+            return
+        }
+
+        startObservingVolume()
+    }
+
+    private func startObservingVolume() {
+        volumeObservation = audioSession.observe(\.outputVolume, options: [.initial, .new]) { [weak self] audioSession, _ in
+            let currentVolume = audioSession.outputVolume
+            self?.volumeDidChange(currentVolume)
+        }
+    }
+
+    func volumeDidChange(volume: Float) {
+        print("Volume did change: \(volume)")
+        // 在此处执行音量更改后的操作
+    }
+
+    func stopObservingVolume() {
+        volumeObservation = nil
+    }
+
+    deinit {
+        stopObservingVolume()
+    }
+}
