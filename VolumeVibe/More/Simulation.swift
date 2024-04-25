4cfd4bd701b7659d005bd5595d11b356941b53e8
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-04-25 23:29:46 +0800

    .

--- VolumeVibe/More/Simulation.swift
+++ VolumeVibe/More/Simulation.swift
@@ -1,7 +1,38 @@
 import SwiftUI
+import AVFoundation
 
 struct Simulation: View {
+    @State private var isPlaying = false
+    private var audioPlayer: AVAudioPlayer?
+
+    init() {
+        if let audioURL = Bundle.main.url(forResource: "Castle in the Sky", withExtension: "mp3") {
+            do {
+                self.audioPlayer = try AVAudioPlayer(contentsOf: audioURL)
+            } catch {
+                print("Error: \(error.localizedDescription)")
+            }
+        } else {
+            print("Error: Audio file not found.")
+        }
+    }
+
     var body: some View {
-        EmptyView()
+        VStack {
+            Button(action: {
+                if self.isPlaying {
+                    self.audioPlayer?.pause()
+                } else {
+                    self.audioPlayer?.play()
+                }
+                self.isPlaying.toggle()
+            }) {
+                Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.blue)
+            }
+        }
     }
 }
