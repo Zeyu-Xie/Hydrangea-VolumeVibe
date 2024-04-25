b67badabd686f4b8772b75cd0cb24d16ab74456f
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-04-26 00:04:24 +0800

    .

--- VolumeVibe/More/Accurate.swift
+++ VolumeVibe/More/Accurate.swift
@@ -1,10 +1,3 @@
-//
-//  Accurate.swift
-//  VolumeVibe
-//
-//  Created by Acan Xie on 2024/4/25.
-//
-
 import SwiftUI
 
 struct Accurate: View {
@@ -15,37 +8,57 @@ struct Accurate: View {
         VStack(spacing: 20) {
             Image(systemName: "speaker.wave.2.circle")
                 .font(.system(size: 60))
-                .foregroundStyle(.blue)
+                .foregroundColor(.blue)
             
             Text("VolumeVibe")
                 .font(.title3)
                 .bold()
             
             Text("Acan Xie")
-                .foregroundStyle(.secondary)
+                .foregroundColor(.secondary)
             
-            Text("Input a value between 0 and 1.")
-                .foregroundStyle(.secondary)
+            VStack(alignment: .leading, content: {
+                Text("On Apple devices, the volume value typically ranges from 0 to 1, where 0 represents muted or no sound, and 1 indicates the maximum volume.").foregroundStyle(.secondary).multilineTextAlignment(.leading)
+                Spacer().frame(maxHeight: 20)
+                Text("This range allows for precise control over the audio output level. It's important to note that values outside this range might not have any effect on the actual volume level.").foregroundStyle(.secondary).multilineTextAlignment(.leading)
+                Spacer().frame(maxHeight: 20)
+                Text("Additionally, some devices might have slightly different behavior or additional features related to volume control.")
+                    .foregroundColor(.secondary).multilineTextAlignment(.leading)
+            })
             
-            TextField("Enter Volume", text: $volumeInput)
-                .padding(8)
-                .background(Color.white)
+            TextField("Set Volume 0 - 1", text: $volumeInput)
+                .padding(12)
+                .background(Color(UIColor.systemBackground))
                 .cornerRadius(10)
+                .overlay(
+                    HStack {
+                        Text("Volume")
+                            .foregroundColor(.black)
+                            .padding(.horizontal, 20)
+                        Spacer()
+                    }
+                )
                 .keyboardType(.decimalPad)
+            
+            
             
             Button(action: {
                 setSysVolum(Float(volumeInput.replacingOccurrences(of: ",", with: "."))!)
             }) {
                 Text("Set Volume")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
             }
-            .buttonStyle(FilledButtonStyle())
             
             Spacer()
         }
         .padding()
         .frame(maxHeight: .infinity)
         .multilineTextAlignment(.center)
-        .background(Color(.systemGray6).ignoresSafeArea())
+        .background(Color(UIColor.systemGray6).ignoresSafeArea())
         .onTapGesture {
             hideKeyboard()
         }
@@ -55,62 +68,3 @@ struct Accurate: View {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
-
-struct FilledButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .foregroundColor(.white)
-            .padding()
-            .frame(maxWidth: .infinity)
-            .background(configuration.isPressed ? Color.blue.opacity(0.8) : Color.blue)
-            .cornerRadius(10)
-    }
-}
-
-
-////
-////  Accurate.swift
-////  VolumeVibe
-////
-////  Created by 谢泽钰 on 2024/4/25.
-////
-//
-//import Foundation
-//import SwiftUI
-//
-//struct Accurate: View {
-//    
-//    @State private var vo:String = ""
-//    
-//    var body: some View {
-//        VStack {
-//            Image(systemName: "speaker.wave.2.circle").font(.system(size: 60)).foregroundStyle(.blue)
-//            Spacer().frame(maxHeight: 20)
-//            Text("VolumeVibe").font(.title3).bold()
-//            Text("Acan Xie").foregroundStyle(.secondary)
-//            Spacer().frame(maxHeight: 30)
-//            Text("Input a value between 0 and 1.").foregroundStyle(.secondary)
-//            Spacer().frame(maxHeight: 20)
-//            TextField( "", text: $vo).padding(8)
-//                .background(.white) // 设置背景颜色
-//                .cornerRadius(10) // 设置圆角
-//                .padding().keyboardType(.decimalPad)
-//            Spacer().frame(maxHeight: 0)
-//            Button(action: {
-//                let vo_num:Float = Float(vo.replacingOccurrences(of: ",", with: ".")) ?? -1
-//                if(vo_num >= 0) {
-//                    setSysVolum(Float(vo_num))
-//                }
-//            }) {
-//                Text("Set Volume")
-//            }
-//        }
-//        .frame(maxHeight: .infinity, alignment: .center)
-//        .multilineTextAlignment(.center)
-//        .onTapGesture {
-//            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-//        }
-//        .background(Color(.systemGray6))
-//        
-//    }
-//}
